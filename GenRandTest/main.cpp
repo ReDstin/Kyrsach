cf934515b3531633031106c5fa354a757e1816b8
ReDstin <72280563+ReDstin@users.noreply.github.com>
2021-05-27 23:12:40 +0300

    Update main.cpp

--- GenRandTest/main.cpp
+++ GenRandTest/main.cpp
@@ -16,28 +16,28 @@ struct GenRand_fixture {
 SUITE(KeyTest)
 {
 	vector<int> reg1 = {1,1,0,1,0,0,1,0,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,1};
-	TEST_FIXTURE(GenRand_fixture, CorrectData){
+	TEST_FIXTURE(GenRand_fixture, CorrectReg){
 		object->setReg(reg1);
 		CHECK(true);
 	}
 	vector<int> reg2 = {1,1,0,1,0,0,1,0,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,1,1,0,0,1,1,0,1,1,1,0,0,1};
-	TEST_FIXTURE(GenRand_fixture, ZeroLength){
+	TEST_FIXTURE(GenRand_fixture, reg>max){
 		CHECK_THROW(object->setReg(reg2), GenRandError);
 	}
 	vector<int> reg3 = {1,1,0,1,0,0,1,0,1,1,0,1};
-	TEST_FIXTURE(GenRand_fixture, InvalidCharacter){
+	TEST_FIXTURE(GenRand_fixture, reg<min)
 		CHECK_THROW(object->setReg(reg3), GenRandError);
 	}
 	vector<int> reg4 = {};
-	TEST_FIXTURE(GenRand_fixture, ZeroCase){
+	TEST_FIXTURE(GenRand_fixture, Zeroreg)
 		CHECK_THROW(object->setReg(reg4), GenRandError);
 	}
 	vector<int> reg5 = {1,1,0,1,6,9,1,0,1,1,0,1,1,1,1,0,0,5,1,0,5,5,1,1};
-	TEST_FIXTURE(GenRand_fixture, InvalidCharactfer){
+	TEST_FIXTURE(GenRand_fixture, Invalidsimvol)
 		CHECK_THROW(object->setReg(reg5), GenRandError);
 	}
 	vector<int> reg6 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	TEST_FIXTURE(GenRand_fixture, CaseIsShort){
+	TEST_FIXTURE(GenRand_fixture, regzero)
 		CHECK_THROW(object->setReg(reg6), GenRandError);
 	}
 	
@@ -45,14 +45,14 @@ SUITE(KeyTest)
 
 
 SUITE(GenerateTest) {
-	TEST_FIXTURE(GenRand_fixture, CorrectData){
+	TEST_FIXTURE(GenRand_fixture, Correctsize)
 		object->gen(5);
 		CHECK(true);
 	}
-	TEST_FIXTURE(GenRand_fixture, ZeroLength){
+	TEST_FIXTURE(GenRand_fixture, Zerosize)
 		CHECK_THROW(object->gen(0), GenRandError);
 	}
-	TEST_FIXTURE(GenRand_fixture, ThereIsLetter){
+	TEST_FIXTURE(GenRand_fixture, sizeinvalid)
 		CHECK_THROW(object->gen(-5), GenRandError);
 	}
 }
